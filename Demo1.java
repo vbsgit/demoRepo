20ae77937a8398735f0fa78cefa9077d435f8684
svbme <svbharat@yahoo.co.in>
2021-02-03 10:38:14 +0000

    Commit all the files Demo1 demo2 and class file

--- Demo1.java
+++ Demo1.java
@@ -1,6 +1,7 @@
 class Demo1{
 	public static void main(String args[])
 	{
-		System.out.println("Java Demo class1")
+		System.out.println("Java Demo class1");
+		System.out.println("This is the second line in Demo1.java file");
 	}
 }
