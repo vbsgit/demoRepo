5b006470a5ede69a1c706fba3d579f754205b68a
svbme <svbharat@yahoo.co.in>
2021-02-09 14:02:42 +0000

    added a new line in demo3 for master

--- Demo3.java
+++ Demo3.java
@@ -3,6 +3,7 @@ class Demo3{
 	{
 		System.out.println("Java Demo class3 added in remote repo");
 	        System.out.println("Java Demo class3 added for feature 1");
+ 	        System.out.println("Java Demo class3 added new line for master");
 
 
 	}
