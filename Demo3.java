b97ceaee6e324179203da5844f3ec7dbeecbe078
svbme <svbharat@yahoo.co.in>
2021-02-09 13:44:02 +0000

    add a second line for feature1 branch

--- Demo3.java
+++ Demo3.java
@@ -2,6 +2,8 @@ class Demo3{
 	public static void main(String args[])
 	{
 		System.out.println("Java Demo class3 added in remote repo");
+	        System.out.println("Java Demo class3 added for feature 1");
+
 
 	}
 }
