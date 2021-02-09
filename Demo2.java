c30ff95efd8fe2fefc7ff76ff5087ac225fbb91e
svbme <svbharat@yahoo.co.in>
2021-02-09 13:45:34 +0000

    add second line for feature1

--- Demo2.java
+++ Demo2.java
@@ -2,6 +2,7 @@ class Demo2{
 	public static void main(String args[])
 	{
 		System.out.println("Java Demo class2");
+		 System.out.println("Java Demo class2 .add second line for feature 2");
 
 	}
 }
