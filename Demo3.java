69b2d7d85d64a50f78dd16b6365611c1125a687b
svbme <svbharat@yahoo.co.in>
2021-02-09 14:20:04 +0000

    Updated line1 in Demo3.java for feature3

--- Demo3.java
+++ Demo3.java
@@ -1,7 +1,7 @@
 class Demo3{
 	public static void main(String args[])
 	{
-		System.out.println("Java Demo class3 added in remote repo");
+		System.out.println("Java Demo class3 .Update the line for feature3");
 	        System.out.println("Java Demo class3 added for feature 1");
 		System.out.println("Java Demo class3 added for feature3");
 
